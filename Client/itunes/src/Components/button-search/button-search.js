b5228dae26973a841290d759b9dd773c09c6f255
liormiz <liorstrongxd>
2019-12-01 10:11:44 +0200

    aaa

--- Client/itunes/src/Components/button-search/button-search.js
+++ Client/itunes/src/Components/button-search/button-search.js
@@ -4,41 +4,49 @@ import './button-search.css';
 
 class ButtonSearch extends Component {
     constructor(props) {
-       super(props)
+       super(props);
+       this.getTopTenItunes = this.getTopTenItunes.bind(this);
+       this.getTopTenSearching = this.getTopTenSearching.bind(this);
+       this.search = this.search.bind(this);
+    }
+
+    async getTopTenItunes(e){
+        const res = await fetch('http://localhost:9000/itunes/topten');
+        var data = await res.json();
+        this.props.getSearchData(data.results);
+    }
+
+    async getTopTenSearching(e){
+        const res = await fetch('http://localhost:9000/queries/topten');
+        var data = await res.json();
+        let queries = "";
+        for (var i =0; i < data.length; i++){
+            queries += data[i] + ", ";
+        }
+
+        queries = queries.slice(0, queries.length - 2);
+        this.props.getTopTenQueries(queries);
+    }
+
+    async search(e){
+        if (this.state && this.state.searchText)
+        {
+            const res = await fetch('http://localhost:9000/itunes/search/' + this.state.searchText);
+            var data = await res.json();
+            this.props.getSearchData(data.results);
+        }
+        else {
+            alert("u need to choose ur Search");
+        }
     }
 
     render() { 
         return (
            <div>
-            <button className="btn btn-primary margin-right" type="submit" onClick={async (e) =>{
-                    const res = await fetch('http://localhost:9000/itunes/topten');
-                    var data = await res.json();
-                    this.props.getSearchData(data.results);
-            }}>top ten itunes</button>
+            <button className="btn btn-primary margin-right" type="submit" onClick={this.getTopTenItunes}>top ten itunes</button>
             <div className="btn-group">
-            <button className="btn btn-primary margin-right" type="submit" onClick={async (e) =>{
-                    const res = await fetch('http://localhost:9000/queries/topten');
-                    var data = await res.json();
-                    let queries = "";
-                    for (var i =0; i < data.length; i++){
-                        queries += data[i] + ", ";
-                    }
-
-                    queries = queries.slice(0, queries.length - 2);
-                    this.props.getTopTenQueries(queries);
-
-            }}>top ten searching</button>
-            <button className="btn btn-primary" type="submit" onClick={async (e) =>{
-                if (this.state && this.state.searchText)
-                {
-                    const res = await fetch('http://localhost:9000/itunes/search/' + this.state.searchText);
-                    var data = await res.json();
-                    this.props.getSearchData(data.results);
-                }
-                else {
-                    alert("u need to choose ur Search");
-                }
-            }}>search</button>
+            <button className="btn btn-primary margin-right" type="submit" onClick={this.getTopTenSearching}>top ten searching</button>
+            <button className="btn btn-primary" type="submit" onClick={this.search}>search</button>
             <input type="text" className="margin-left" name="text" onChange={(e) => { this.setState({ searchText: e.target.value }) }}/>
             </div>
            </div>
